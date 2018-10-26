3d6c037ab5ba15801260a598f2f592d1b1affd08
Valentin Stavetski <vstavetski@gmail.com>
2018-10-26 17:25:03 +0300

    update confirmation popup

--- src/components/ConfirmationPopup/ConfirmationPopup.js
+++ src/components/ConfirmationPopup/ConfirmationPopup.js
@@ -1,27 +1,40 @@
 import React from 'react';
 
 import './ConfirmationPopup.css';
+import Block, {BlockRow} from "../Settings/Block";
+import Titile from "../Titile/Titile";
+import Button from "../Button/Button";
 
-const ConfirmationPopup = ({children, content }) => (
-	<div className='confirmation-popup'>
-		<div className='confirmation-popup__popup'>
-			<div className='confirmation-popup__content'>
-				confirm transactions?
-				{content}
-			</div>
-			<div className='confirmation-popup__buttons'>
-				{children}
-			</div>
-		</div>
-	</div>
+const ConfirmationPopup = ({from, to, approveCallback, rejectCallback, children, content}) => (
+    <div className='confirmation-popup'>
+        <Titile inline={true}>Transaction confirmation</Titile>
+        <Block>
+            <BlockRow>
+                <div className='confirmation-popup__popup'>
+                    <BlockRow>
+                        <div className='popup-label'>Sender address:</div>
+                        {from}
+                    </BlockRow>
+
+                    <BlockRow>
+                        <div className='popup-label'>Recipient address:</div>
+                        {to}
+                    </BlockRow>
+                    {content}
+                </div>
+                <div className='confirmation-popup__buttons'>
+                    <Button color='green' onClick={approveCallback}>Confirm</Button>
+                    <Button color='red' onClick={rejectCallback}>Reject</Button>
+                </div>
+            </BlockRow>
+        </Block>
+    </div>
 );
 
-export const ApproveButton = (props) => (
-	<button {...props} />
-);
-
-export const RejectButton = (props) => (
-	<button {...props} />
+export const TxDetailsContainer = ({children}) => (
+    <div className='tx-details-container'>
+        {children}
+    </div>
 );
 
 export default ConfirmationPopup;
--- src/containers/Application/ConfirmationPopup.js
+++ src/containers/Application/ConfirmationPopup.js
@@ -1,66 +1,96 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {connect} from "react-redux";
 import ConfirmationPopup, {ApproveButton} from './../../components/ConfirmationPopup/ConfirmationPopup';
 import {approve, reject} from './../../redux/wallet';
+import Input from "../../components/Input/Input";
+import {TxDetailsContainer} from "../../components/ConfirmationPopup/ConfirmationPopup";
+import web3utils from 'web3-utils';
 
 
 class ConfirmationPopupContainer extends Component {
     approve = () => {
-        const gas = this.refs.gas.value;
-        const gasPrice = this.refs.gasPrice.value;
-        this.props.approve(gas, gasPrice);
-    }
+        const gasLimit = this.gasLimit.value;
+        const gasPrice = this.gasPrice.value;
+        this.props.approve(gasLimit, gasPrice);
+    };
 
     reject = () => {
         this.props.reject();
-    }
+    };
 
-	render() {
-		const {
+    render() {
+        const {
             pendingRequest,
             request,
-		} = this.props;
+        } = this.props;
 
         let _from;
         let _to;
+        let _gasLimit;
+        let _gasPrice;
+        let _amount;
+
+        debugger;
+
         if (request) {
-            _from = request.params[0].from
+            const value = request.params[0].value;
+            _from = request.params[0].from;
             _to = request.params[0].to;
+            _gasLimit = web3utils.hexToNumber(request.params[0].gas);
+            _gasPrice = web3utils.hexToNumber(request.params[0].gasPrice);
+            _amount = value ? web3utils.hexToNumber(value) : 0;
         }
 
-		return (
-			<div>
-				{pendingRequest && <ConfirmationPopup
-		              content={<div>  
-		                <div>
-		                gas limit: <input ref='gas' placeholder='gas' defaultValue={7200000}/>
-		                gas price: <input ref='gasPrice' placeholder='gas price' defaultValue={12}/>
-		                </div>
-		                <div>
-		                from:{_from}
-		                </div>
-		                <div>
-		                to:{_to}
-		                </div>
-		            </div>}
-		            >
-		                <ApproveButton onClick={this.approve}>approve</ApproveButton>
-		                <ApproveButton onClick={this.reject}>reject</ApproveButton>
-		            </ConfirmationPopup>
-		        }	
-			</div>
-		)
-	}
+        return (
+            <div>
+                {pendingRequest &&
+                <ConfirmationPopup
+                    from={_from}
+                    to={_to}
+                    approveCallback={this.approve}
+                    rejectCallback={this.reject}
+                    content={
+                        <TxDetailsContainer>
+                            <span>
+                                <div className='popup-label'>Amount (ETH):</div>
+                                <Input
+                                    defaultValue={_amount}
+                                    inputRef={node => {this.ethAmount = node}}
+                                    style={{width: 100}}
+                                />
+                            </span>
+                            <span>
+                                <div className='popup-label'>Gas price (GWEI):</div>
+                                <Input
+                                    defaultValue={_gasPrice}
+                                    inputRef={node => {this.gasPrice = node}}
+                                    style={{width: 100}}
+                                />
+                            </span>
+                            <span>
+                                <div className='popup-label'>Gas limit:</div>
+                                <Input
+                                    defaultValue={_gasLimit}
+                                    inputRef={node => {this.gasLimit = node}}
+                                    style={{width: 100}}/>
+                            </span>
+                        </TxDetailsContainer>
+                    }
+                />
+                }
+            </div>
+        )
+    }
 }
 
 export default connect(
-	state => ({
+    state => ({
         pendingRequest: state.wallet.pendingRequest,
         request: state.wallet.request,
-	}),
-	{ 
+    }),
+    {
         approve,
         reject,
-	}
+    }
 )(ConfirmationPopupContainer);
 
