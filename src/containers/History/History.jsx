0633d7cfed59e7b3e55579a5bd6461d1995ba6d0
Andrey <luzkovskyandrey@gmail.com>
2018-12-03 18:30:11 +0300

    ui for history

--- src/containers/History/History.jsx
+++ src/containers/History/History.jsx
@@ -1,32 +1,54 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Title } from '@cybercongress/ui';
+
+import CybLink from '../../components/CybLink';
+import RootRegistry, { Table } from '../../components/RootRegistry/RootRegistry';
 
 
 class History extends Component {
-    back = () => {
+    renderItem = (dura, index) => {
+        let content = (
+            <CybLink dura={dura}>{dura}</CybLink>
+        );
 
-        const { history } = this.props;
-        if (history.length > 0) {
-            const lastUrl = history[history.length - 1];
-
-            // dispatch(navigate(lastUrl));
-            this.props.dispatch({ type: 'MOVE_BACK' });
+        if (dura === 'history.cyb') {
+            content = (
+                <span>
+                    {dura}
+                </span>
+            );
         }
+
+        return (
+            <tr key={index}>
+                <td>{content}</td>
+                <td>7/2/2018 17:33:12</td>
+            </tr>
+        );
     }
 
     render() {
         const { history } = this.props;
+        const _history = history.slice(0, history.length - 1);
+
+        _history.reverse();
+
         return (
-            <div>
-                <div>
-                {history.map((item, index) => (
-                    <div key={index}>
-                        {item}
-                    </div>
-                ))}
-                </div>
-                <button onClick={this.back}>back</button>
-            </div>
+            <RootRegistry>
+                <Title>/History</Title>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>Address</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {_history.map(this.renderItem)}
+                    </tbody>
+                </Table>
+            </RootRegistry>
         );
     }
 }
@@ -34,5 +56,5 @@ class History extends Component {
 export default connect(
     state => ({
         history: state.browser.history,
-    })
+    }),
 )(History);
