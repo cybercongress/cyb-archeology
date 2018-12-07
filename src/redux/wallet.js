59f6d4b099bb599dfb890d6775f023409152f3b0
Andrey <luzkovskyandrey@gmail.com>
2018-12-07 18:13:51 +0300

    hide sing popup after approve

--- src/redux/wallet.js
+++ src/redux/wallet.js
@@ -344,7 +344,7 @@ export const approve = (gasLimit, gasPrice) => (dispatch, getState) => {
             });
          //   dispatch(hidePending());
         }
-        // dispatch(hidePending());
+        dispatch(hidePending());
     });
 };
 
