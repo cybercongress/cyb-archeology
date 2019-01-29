1d0372920b528e7bfad64490e2786375673fe952
Valentin Stavetski <vstavetski@gmail.com>
2019-01-29 13:19:26 +0700

    move help app to cyb (#121)

--- src/router.jsx
+++ src/router.jsx
@@ -12,6 +12,16 @@ import Home from './containers/Home';
 import TransactionView from './containers/TransactionView/TransactionView';
 import History from './containers/History/History';
 import TxQueue from './containers/TxQueue/TxQueue';
+import HelpHome from './containers/Help/HelpHome';
+import Create from './containers/Help/Create';
+import Deploy from './containers/Help/Deploy';
+import Open from './containers/Help/Open';
+import Ipfs from './containers/Help/Ipfs';
+import HelpEth from './containers/Help/HelpEth';
+import Favourites from './containers/Help/Favourites';
+import Switch from './containers/Help/Switch';
+import HelpLink from './containers/Help/HelpLink';
+import HelpApp from './containers/Help/HelpApp';
 
 const AppRouter = () => (
     <Router history={ hashHistory }>
@@ -24,6 +34,20 @@ const AppRouter = () => (
             <Route path='/txq' component={ TxQueue } />
             <Route path='/notfound' component={ NotFound } />
             <Route path='/browser' component={ Browser } />
+
+            <Route path='/help' component={ HelpApp }>
+                <Route path='/help/create' component={ Create } />
+                <Route path='/help/deploy' component={ Deploy } />
+                <Route path='/help/open' component={ Open } />
+                <Route path='/help/ipfs' component={ Ipfs } />
+                <Route path='/help/eth' component={ HelpEth } />
+                <Route path='/help/favourites' component={ Favourites } />
+                <Route path='/help/switch' component={ Switch } />
+                <Route path='/help/link' component={ HelpLink } />
+
+                <IndexRoute component={ HelpHome } />
+            </Route>
+
             <IndexRoute component={ Home } />
             <Route path='*' exact component={ NotFound } />
         </Route>
