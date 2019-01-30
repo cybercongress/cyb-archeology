244583b3f1fa4f0202a20f74368f2b8b8511f6aa
dimakorzhovnik <korzhovnik.dima@gmail.com>
2019-01-30 20:50:51 +0300

    fix EthAccounts fix CyberAccounts

--- src/containers/Wallet/CyberAccounts.jsx
+++ src/containers/Wallet/CyberAccounts.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import connect from 'react-redux/es/connect/connect';
-import * as cyberActions from '../../redux/cyber';
 import {
+    Button,
+    AccountCardLeft,
+    AccountCardRight,
+    AccountCardContent,
+    AccountCardContentItem,
+    MainIndecator,
+    CreateButtonContainer,
+    AccountCard,
     Avatar,
-} from '../../components/Wallet/Wallet';
-import Button from '../../components/Button/Button';
+} from '@cybercongress/ui';
+import * as cyberActions from '../../redux/cyber';
+// import {
+//     Avatar,
+// } from '../../components/Wallet/Wallet';
+// import Button from '../../components/Button/Button';
 
-import AccountCard, {
-    AccountCardLeft, AccountCardRight,
-    AccountCardContent, AccountCardContentItem,
-    MainIndecator, SelectButton, CreateButtonContainer,
-} from '../../components/Wallet/AccountCard/AccountCard';
-
+// import AccountCard, {
+//     AccountCardLeft, AccountCardRight,
+//     AccountCardContent, AccountCardContentItem,
+//     MainIndecator, SelectButton, CreateButtonContainer,
+// } from '../../components/Wallet/AccountCard/AccountCard';
 
 const CyberAccounts = (props) => {
-    const {
-        accounts, defaultAccount, defaultAccountBalance,
-    } = props;
+    const { accounts, defaultAccount, defaultAccountBalance } = props;
 
     const defaultAccountComponent = defaultAccount && (
         <AccountCard>
@@ -27,21 +35,20 @@ const CyberAccounts = (props) => {
             <AccountCardRight>
                 <AccountCardContent>
                     <AccountCardContentItem>
-                    Address:
-                        {' '}
+Address:
                         {defaultAccount}
                     </AccountCardContentItem>
                     <AccountCardContentItem>
                         <div>
-                            Balance:
+Balance:
                             {' '}
                             {defaultAccountBalance}
                             {' '}
-                            CYB
+CYB
                         </div>
                         <div>
-                            <Button onClick={ () => props.onCopyKey(defaultAccount) }>
-                            COPY PRIVATE KEY
+                            <Button color='blue' onClick={ () => props.onCopyKey(defaultAccount) }>
+                                COPY PRIVATE KEY
                             </Button>
                         </div>
                     </AccountCardContentItem>
@@ -54,26 +61,27 @@ const CyberAccounts = (props) => {
         <AccountCard key={ account.address }>
             <AccountCardLeft>
                 <Avatar hash={ account.address } />
-                <SelectButton
+                <Button
+                  color='ogange'
+                  style={ { fontSize: 14 } }
                   onClick={ () => props.setDefaultCyberAccount(account) }
                 >
                     MAKE MAIN
-                </SelectButton>
+                </Button>
             </AccountCardLeft>
             <AccountCardRight>
                 <AccountCardContent>
                     <AccountCardContentItem>
-                        Address:
-                        {' '}
+Address:
                         {account.address}
                     </AccountCardContentItem>
                     <AccountCardContentItem>
                         <div>
-                            Balance:
+Balance:
                             {' '}
                             {account.balance}
                             {' '}
-                            CYB
+CYB
                         </div>
                         <div>
                             <Button
@@ -83,8 +91,8 @@ const CyberAccounts = (props) => {
                             >
                                 REMOVE
                             </Button>
-                            <Button onClick={ () => props.onCopyKey(account.address) }>
-                            COPY PRIVATE KEY
+                            <Button color='blue' onClick={ () => props.onCopyKey(account.address) }>
+                                COPY PRIVATE KEY
                             </Button>
                         </div>
                     </AccountCardContentItem>
@@ -97,12 +105,14 @@ const CyberAccounts = (props) => {
         <div>
             {defaultAccountComponent}
 
-            {defaultAccount && <hr />}
+            {defaultAccount && <hr style={ { marginBottom: 10 } } />}
 
             {accountsItem}
 
             <CreateButtonContainer>
-                <Button onClick={ () => props.createCyberAccount() }>CREATE NEW</Button>
+                <Button color='blue' onClick={ () => props.createCyberAccount() }>
+                    CREATE NEW
+                </Button>
             </CreateButtonContainer>
         </div>
     );
