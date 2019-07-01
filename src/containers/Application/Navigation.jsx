ff3e4da1d5299856559e49556fceea03cb7df1c2
dimakorzhovnik <korzhovnik.dima@gmail.com>
2019-07-01 16:46:34 +0300

    add new design  to  popup "add new app"

--- src/containers/Application/Navigation.jsx
+++ src/containers/Application/Navigation.jsx
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {
     SearchInput,
-    BackButton, ForwardButton, NavigationContainer,
+    BackButton,
+    ForwardButton,
+    NavigationContainer,
     FavoriteButton,
     FavoriteButtonContainer,
     SearchIcon,
+    Popover,
+    Button,
+    Pane,
+    TextInput,
+    IconButton,
+    Text,
 } from '@cybercongress/ui';
 import { navigate, goBack, canBack } from '../../redux/browser';
 // import SearchInput, {
@@ -50,19 +58,93 @@ class Navigation extends Component {
     };
 
     render() {
-        const {
-            inputValue,
-        } = this.state;
-        const {
-            dura, canBack, isFavorited,
-        } = this.props;
+        const { inputValue } = this.state;
+        const { dura, canBack, isFavorited } = this.props;
         const homePage = dura === '';
+
+        const AddNewApp = () => (
+            <Pane
+              paddingY={ 20 }
+              paddingX={ 20 }
+              display='flex'
+              flexDirection='column'
+              backgroundColor='#000'
+            >
+                <Text display='inline-block' color='#fff' marginBottom={ 10 }>
+                    New App
+                </Text>
+                <Pane display='flex' marginBottom={ 10 }>
+                    <TextInput
+                      autoFocus
+                      width='100%'
+                      defaultValue='New App'
+                      background='transparent'
+                      className='input-green-no-focus'
+                    />
+                    <IconButton
+                      marginLeft={ 10 }
+                      className='btn'
+                      paddingX={ 6 }
+                      icon='tick'
+                      intent='success'
+                    />
+                </Pane>
+                <Text display='inline-block' fontSize={ 10 } color='#38d6ae'>
+                    QmT26rKxQdkD6XruyN8J3LDkRxWy6ZAjeYabfQUe9GssGf
+                </Text>
+            </Pane>
+        );
+
+        const RenameApp = () => (
+            <Pane
+              paddingY={ 20 }
+              paddingX={ 20 }
+              display='flex'
+              flexDirection='column'
+              backgroundColor='#000'
+            >
+                <Text display='inline-block' color='#fff' marginBottom={ 6 }>
+                    Rename App
+                </Text>
+                <Pane display='flex'>
+                    <TextInput
+                      autoFocus
+                      width='100%'
+                      defaultValue={ dura }
+                      background='transparent'
+                      className='input-green-no-focus'
+                    />
+                    <IconButton
+                      marginLeft={ 10 }
+                      className='btn'
+                      paddingX={ 6 }
+                      icon='tick'
+                      intent='success'
+                    />
+                </Pane>
+                <Button
+                  marginTop={ 20 }
+                  iconBefore='trash'
+                  className='btn'
+                  width='fit-content'
+                  intent='danger'
+                >
+                    Delete App
+                </Button>
+            </Pane>
+        );
 
         return (
             <NavigationContainer>
-                { !homePage && <BackButton style={{ padding: '0 10px', cursor: 'pointer'}} disabled={ !canBack } onClick={ this.props.goBack } /> }
+                {!homePage && (
+                    <BackButton
+                      style={ { padding: '0 10px', cursor: 'pointer' } }
+                      disabled={ !canBack }
+                      onClick={ this.props.goBack }
+                    />
+                )}
                 <FavoriteButtonContainer>
-                    { !!homePage && <SearchIcon /> }
+                    {!!homePage && <SearchIcon />}
                     <SearchInput
                       textAlign='center'
                       width='100%'
@@ -72,14 +154,21 @@ class Navigation extends Component {
                       onChange={ e => this.onSearchStringChange(e) }
                       onKeyPress={ this._handleKeyPress }
                     />
-                    { !homePage && (
-                    <FavoriteButton
-                      isFavorited={ isFavorited }
-                      onClick={ this.props.toggleFavorited }
-                    />
-) }
+                    {!homePage && (
+                        <Popover
+                          bringFocusInside
+                          content={ isFavorited ? <RenameApp /> : <AddNewApp /> }
+                        >
+                            <FavoriteButton
+                              isFavorited={ isFavorited }
+                              onClick={ this.props.toggleFavorited }
+                            />
+                        </Popover>
+                    )}
                 </FavoriteButtonContainer>
-                { !homePage && <ForwardButton style={{ padding: '0 10px', cursor: 'pointer'}} disabled /> }
+                {!homePage && (
+                    <ForwardButton style={ { padding: '0 10px', cursor: 'pointer' } } disabled />
+                )}
             </NavigationContainer>
         );
     }
